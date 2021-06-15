a0ed1709158b76148f388a8cb9fd9d5924ff582f
alestor123 <alestoraldous@gmail.com>
2021-06-15 20:58:02 +0530

    inited npm

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 const grammer = require('./App')
+const {readFileSync} = require('fs')
 const chalk = require('chalk');
 (async () => {
-(await grammer('this is aawesome')).matches.forEach(err => console.log(chalk.redBright.bold(err.message)))
+(await grammer(readFileSync(process.argv[2]).toString())).matches.forEach(err => console.log(chalk.redBright.bold(err.message)))
 })()
